3924b59486c6573d9d59385ad2f4058a34e58519
esmanurbtmz <esmabitmez@hotmail.com>
2023-02-20 20:59:53 +0300

    project started

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import { library} from '@fortawesome/fontawesome-svg-core';
+import {fas} from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).mount('#app')
+library.add(fas)
+
+const app = createApp(App)
+app.mount('#app')
---
+++ src/view/MainPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="col-lg-2 aside">
+            <ul>
+                <li class="mt-5 me-3"><a href="">Ana Sayfa</a></li>
+                <li class="mt-3 me-3"><a href="">Stoklar</a></li>
+               
+
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+
+}
+</script>
+<style scoped>
+.aside{
+    background-color: #023047;
+    position: fixed;
+    height: 100%;
+}
+ul li{
+    text-decoration: none;
+    list-style: none;
+    
+}
+ul li a{
+    text-decoration: none;
+    color: aliceblue;
+}
+ul li a:hover{
+    color:rgb(163, 208, 247);
+}
+i{
+    font-size: 30px;
+}
+</style>
